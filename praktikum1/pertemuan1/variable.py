691c64ea41d81cbc731644a040b53f2df230f3ad
Andriansyah <114373615+Andriansyah17@users.noreply.github.com>
2022-12-07 21:24:28 +0700

    Add files via upload

---
+++ praktikum1/pertemuan1/variable.py
@@ -0,0 +1,11 @@
+namaLengkap = "Andriansyah" # string
+asal = "Bogor" # string
+umur = 19 # integer
+hobi = "traveling" # string
+isStudent = True # boolean
+
+
+print("Hallo Nama Saya :", namaLengkap)
+print("Saya Tinggal Di :", asal)
+print("Umur saya :", umur)
+print("Hobi saya :", hobi)
